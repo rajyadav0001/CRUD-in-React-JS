27cb9b84e9dbd58459b9f669d8c7652a2e46adbc
Rajnesh Kumar <109650270+rajyadav0001@users.noreply.github.com>
2023-05-31 14:38:13 +0530

    all users listing table styling done

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -1,9 +1,20 @@
- 
+import { Table, TableCell, TableHead, TableRow } from '@mui/material'
 import React from 'react'
 
 const AllUsers = () => {
   return (
     <div>
+       <Table>
+          <TableHead>
+               <TableRow>
+                   <TableCell>Id</TableCell>
+                   <TableCell>Name</TableCell>
+                   <TableCell>Username</TableCell>
+                   <TableCell>Email</TableCell>
+                   <TableCell>Phone</TableCell>
+               </TableRow>
+          </TableHead>
+       </Table>
     </div>
   )
 }
